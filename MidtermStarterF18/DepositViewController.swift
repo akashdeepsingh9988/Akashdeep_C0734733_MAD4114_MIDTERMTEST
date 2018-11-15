188674893c1b9cb52cc085d55388b55683bea6c1
MacStudent <macstudent@128-imac15.local>
2018-11-15 10:47:57 -0500

    deposite code finished

--- MidtermStarterF18/DepositViewController.swift
+++ MidtermStarterF18/DepositViewController.swift
@@ -11,6 +11,10 @@ import CoreData
 
 class DepositViewController: UIViewController {
     var index = 0;
+    var currentBalance  = 0.0
+    var name = ""
+    var id = ""
+     let fetchRequest:NSFetchRequest<Customer> = Customer.fetchRequest()
 var context:NSManagedObjectContext!
     // MARK: Outlets
     // ---------------------
@@ -28,6 +32,7 @@ var context:NSManagedObjectContext!
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         
         self.context = appDelegate.persistentContainer.viewContext
+       
 
         print("You are on the Check Balance screen!")
     }
@@ -46,10 +51,9 @@ var context:NSManagedObjectContext!
         print("check balance button pressed!")
          custid =  customerIdTextBox.text!
        
-        let fetchRequest:NSFetchRequest<Customer> = Customer.fetchRequest()
-        fetchRequest.predicate =  NSPredicate(format: "id == %@", custid)
         
-        do {
+        fetchRequest.predicate =  NSPredicate(format: "id == %@", custid)
+                do {
             
             let results = try self.context.fetch(fetchRequest) as [Customer]
             
@@ -62,7 +66,11 @@ var context:NSManagedObjectContext!
                 for x in results {
                     
                     print("Balance \(x.balance)")
+                    name = x.name!
+                    id = x.id!
+                
                     balanceLabel.text = String(x.balance)
+                    currentBalance = x.balance
                 }
                 
             }
@@ -79,12 +87,93 @@ var context:NSManagedObjectContext!
     
     @IBAction func depositButtonPressed(_ sender: Any) {
         print("you pressed the deposit button!")
-    }
+        var amount = Double(depositAmountTextBox.text!)!
+        var total = currentBalance+amount
+        
+        if(custid == nil || custid == "")
+        {
+             messagesLabel.text = "Error! Please try again later"
+        }
+        
+        else
+        {
+            
+                // Save the user to the database
+                // (Send the INSERT to the database)
+                
+                
+                
+                fetchRequest.predicate = NSPredicate.init(format: "id == %@", custid)
+                let objects = try! context.fetch(fetchRequest)
+                for obj in objects {
+                    context.delete(obj)
+                }
+                
+                do {
+                    try context.save() // <- remember to put this :)
+                } catch {
+                    // Do something... fatalerror
+                }
+            
+        
+       // let fetchRequest:NSFetchRequest<Customer> = Customer.fetchRequest()
+        //fetchRequest.predicate =  NSPredicate(format: "id == %@", custid)
+        let p = Customer(context: self.context)
+            p.id = id
+            p.name = name
+            p.balance  = total
+            print("total =" ,total);
+        
+      
+            do{
+            try self.context.save()
+            print("data saved")
+                messagesLabel.text = "Money Deposited \n Previous Balance : $\(currentBalance) \n Deposited Amount: $\(amount) \n New Balance : $\(total)"
+                
+                
+            
+        }
+            
+        catch {
+            print("Error while saving to database")
+           messagesLabel.text = "Error! Please try again later"
+        }
+            
+        }
+       
+        }
     
     
     @IBAction func showCustomersPressed(_ sender: Any) {
         print("Show customers button pressed!")
+        
+        let fetchRequest:NSFetchRequest<Customer> = Customer.fetchRequest()
+        // like where
+       // fetchRequest.predicate = NSPredicate(format: "email == %@", "michael@gmail.com")
+        
+        do {
+           
+            let results = try self.context.fetch(fetchRequest) as [Customer]
+            
+            // Loop through the database results and output each "row" to the screen
+            print("Number of items in database: \(results.count)")
+            
+            for x in results {
+                 print("Customer Name :  \(x.name!)")
+                print("Customer Id : \(x.id!)")
+               
+                print("Balance $\(x.balance)")
+                 print("--------------------------------")
+            }
+        }
+        catch {
+            print("Error when fetching from database")
+        }
     }
+        
+        
+
+
     
     
     /*
